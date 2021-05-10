0c6904e1383fa5211687796db8155fdfabcf7f4f
AlonHarpaz <Alonzharpaz@gmail.com>
2021-05-10 21:58:08 +0300

    10/05/2021

---
+++ .history/week 1/1.1-primitive data types/1.1-primitive data types_20210423190234.js
@@ -0,0 +1,15 @@
+//! The following exercise contains the following subjects:
+// data types
+
+//* Instructions
+// 1. Create a variable that’s value holds a string.
+let string = 'string';
+
+// 2. Create a variable that its value holds a number.
+let number = 123456;
+// 3. Create a variable that its value holds an object.
+// 4. Create a variable that its value holds a boolean.
+// 5. Create a variable that its value holds null.
+// 5. Create a variable that its value is undefined.
+// 6. Create a variable that its value holds a symbol.
+// Console log the types of your variables.
---
+++ .history/week 1/1.1-primitive data types/1.1-primitive data types_20210510184331.js
@@ -0,0 +1,40 @@
+//! The following exercise contains the following subjects:
+// data types
+
+//! Instructions:
+//* 1. Create a variable that’s value holds a string.
+let string = 'string';
+//?explantion: string it's just a caracters, leeters, words etc
+
+//* 2. Create a variable that its value holds a number.
+let number = 123456;
+
+//* 3. Create a variable that its value holds an object.
+let object = {
+ name: 'alon',
+ age: 30
+};
+//?explantion: object contain a properties and eact propeties hold a valu like ingrediens
+
+//* 4. Create a variable that its value holds a boolean.
+let boolean_true = true;
+let boolean_false = false;
+
+//* 5. Create a variable that its value holds null.
+let emptyValue = null;
+
+// 5. Create a variable that its value is undefined.
+let isUndifiend = undefined;
+
+// 6. Create a variable that its value holds a symbol.
+let symbol = symbol();
+
+// Console log the types of your variables.
+console.log(typeof string)
+console.log(typeof number)
+console.log(typeof object)
+console.log(typeof boolean_true)
+console.log(typeof boolean_false)
+console.log(typeof emptyValue)
+console.log(typeof isUndifiend)
+console.log(typeof symbol)
